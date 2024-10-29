b45c6d2c0f4875b1a4ce5cada2b4a1b42ef07344
Thiago Circuncisão <thiagocircuncisao@gmail.com>
2024-10-29 13:32:41 -0300

    added rest template configuration and refac of currency repository

--- src/main/java/thiagocircuncisao/configuration/WebConfiguration.java
+++ src/main/java/thiagocircuncisao/configuration/WebConfiguration.java
@@ -1,6 +1,8 @@
 package thiagocircuncisao.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,9 @@ public class WebConfiguration implements WebMvcConfigurer {
    public void addViewControllers(ViewControllerRegistry registry) {
        registry.addViewController("/").setViewName("redirect:/swagger-ui/index.html");
    }
-   
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/thiagocircuncisao/repository/impl/CurrencyRepositoryImpl.java
+++ src/main/java/thiagocircuncisao/repository/impl/CurrencyRepositoryImpl.java
@@ -1,41 +1,53 @@
 package thiagocircuncisao.repository.impl;
 
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
-@Data
 @Repository
 public class CurrencyRepositoryImpl {
     @Value("${currency-api.url}")
-    private String currencyApiUrl;
+    private String currencyApiUrl = "";
 
-    public Map getCurrencies() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "application/json;charset=UTF-8");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String url = currencyApiUrl + "?filter=record_date:gte:" + LocalDate.now().minusMonths(6);
+    private final RestTemplate restTemplate;
 
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        return restTemplate.exchange(url, HttpMethod.GET, entity, Map.class).getBody();
+    public CurrencyRepositoryImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public Map getCurrency(String country, String currencyName) {
-        RestTemplate restTemplate = new RestTemplate();
+    private HttpHeaders createHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "application/json;charset=UTF-8");
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+    public Map<String, Object> getCurrencies() {
+        String url = currencyApiUrl + "?filter=record_date:gte:" + LocalDate.now().minusMonths(6);
+        HttpEntity<String> entity = new HttpEntity<>(null, createHeaders());
+
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(url, HttpMethod.GET,
+                entity, new ParameterizedTypeReference<>() {});
+
+        return response.getBody();
+    }
+
+    public Map<String, Object> getCurrency(String country, String currencyName) {
         String url = currencyApiUrl + "?filter=country:eq:" + country
                 + ",currency:eq:" + currencyName
-                +",record_date:gte:" + LocalDate.now().minusMonths(6);
+                + ",record_date:gte:" + LocalDate.now().minusMonths(6);
 
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        return restTemplate.exchange(url, HttpMethod.GET, entity, Map.class).getBody();
+        HttpEntity<String> entity = new HttpEntity<>(null, createHeaders());
+
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(url, HttpMethod.GET,
+                entity, new ParameterizedTypeReference<>() {});
+
+        return response.getBody();
     }
 }
